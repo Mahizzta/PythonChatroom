c6858dfe82aba91a5603fa0d95e9b0e2df844153
Alexandru Giuran <giuran2007@gmail.com>
2018-12-30 14:28:48 +0100

    final version.

--- Chat-Room-server/client.py
+++ Chat-Room-server/client.py
@@ -31,11 +31,11 @@ def on_closing(event=None):
     send()
 
 top = tkinter.Tk()
-top.title("Chatter")
+top.title("Chat")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
+my_msg.set("Write your messages here.")
 scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 # Following will contain the messages.
 msg_list = tkinter.Listbox(messages_frame, height=15, width=100, yscrollcommand=scrollbar.set)
